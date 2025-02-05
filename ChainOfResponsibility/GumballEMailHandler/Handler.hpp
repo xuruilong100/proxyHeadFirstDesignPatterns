776f965d76cc1c7d7ed48580f5c972c06584e80d
xuruilong100 <xuruilong100@163.com>
2025-02-05 23:07:42 +0800

    remove redundant comments

--- ChainOfResponsibility/GumballEMailHandler/Handler.hpp
+++ ChainOfResponsibility/GumballEMailHandler/Handler.hpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 #include <iostream>
 #include <list>
-#include <memory>
 #include <string>
 #include "Traits.hpp"
 
@@ -35,51 +34,5 @@ void printRequests(const std::list<std::string>& requests) {
         iterator++;
     }
 }
-
-// class Handler {
-//    private:
-//     std::list<std::string> _requests;
-//     std::shared_ptr<Handler> _successor;
-//
-//     void handleAndStore(const std::string& request) {
-//         _requests.push_back(request);
-//     }
-//
-//     void toNextHandler(const std::string& request) {
-//         if (_successor) {
-//             _successor->handleRequest(request);
-//         }
-//     }
-//
-//    public:
-//     explicit Handler(std::shared_ptr<Handler> successor = nullptr)
-//         : _successor(std::move(successor)) {}
-//     virtual ~Handler() = default;
-//
-//     void handleRequest(std::string request) {
-//         std::transform(request.begin(), request.end(), request.begin(),
-//                        tolower);
-//
-//         if (canHandleRequest(request) == true) {
-//             handleAndStore(request);
-//         } else {
-//             toNextHandler(request);
-//         }
-//     }
-//
-//     virtual bool canHandleRequest(const std::string& request) const = 0;
-//
-//     virtual void print() const = 0 {
-//         std::cout << " count=" << _requests.size() << std::endl;
-//         auto iterator = _requests.begin();
-//         while (iterator != _requests.end()) {
-//             std::cout << " - " << (*iterator) << std::endl;
-//             iterator++;
-//         }
-//         if (_successor) {
-//             _successor->print();
-//         }
-//     }
-// };
 }  // namespace
    // HeadFirstDesignPatterns::ChainOfResponsibility::GumballEMailHandler
